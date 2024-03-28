3a382e3fa54562e185bf5c36aabd87fa29fe4708
Shaokang Wang <swang@infinera.com>
2024-03-28 10:45:26 -0400

    adding files

---
+++ BlazorStrich/StrichInterop.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.JSInterop;
+using System.ComponentModel;
+using System.Text.Json;
+
+namespace StrichInterop
+{
+	public class StrichInterop : IAsyncDisposable
+	{
+		private Lazy<Task<IJSObjectReference>> moduleTask;
+
+        private static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = null,
+            DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+            Converters = {
+                //new PolymorphicConverter<ITrace>(),
+                //new PolymorphicConverter<ITransform>(),
+                //new DateTimeConverter(),
+                //new DateTimeOffsetConverter()
+            }
+        };
+
+        public static JsonSerializerOptions SerializerOptions1 => SerializerOptions;
+
+        public StrichInterop(IJSRuntime jsRuntime)
+		{
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorStrich/StrichInterop.js").AsTask());            
+        }
+
+        public async Task initStrich(IJSRuntime jsRuntime)
+        {
+			var module = await moduleTask.Value;
+			await module.InvokeVoidAsync("initializeSDK");			
+		}
+
+        public async Task runStrich(IJSRuntime jsRuntime,BarcodeString ReadBarcode)
+		{
+			var module = await moduleTask.Value;
+			await module.InvokeVoidAsync("startBarcodeScan", DotNetObjectReference.Create(ReadBarcode));
+		}        
+
+        public async ValueTask DebugMessage(string mess)
+		{
+			var module = await moduleTask.Value;
+            await module.InvokeVoidAsync("DisplayMessage", mess);            
+        }
+
+		public async ValueTask DisposeAsync()
+		{			
+			try
+			{
+				if (moduleTask.IsValueCreated)
+				{
+					var module = await moduleTask.Value;					
+					await module.DisposeAsync();
+				}                
+            }
+			catch (JSDisconnectedException ex)
+			{
+				// Ignore
+				await DebugMessage(ex.Message);
+			}
+		}
+
+	}
+
+	public class BarcodeString
+	{
+		private string scanresult;        
+        public string ScanResult
+		{
+			get => scanresult;
+			set => scanresult = value;
+		}
+        [JSInvokable]
+		public void UpdateCodeAsync(string newValue)
+		{
+			scanresult = newValue;			
+		}        
+    }
+}
---
+++ BlazorStrichApp/bin/Release/net6.0/publish/wwwroot/_content/BlazorStrich/StrichInterop.js
@@ -0,0 +1,87 @@
+﻿/**
+ * Add detected code to DOM
+ */
+
+var defaultConfig4BCR = {
+    selector: '#scanner',
+    engine: {
+        symbologies: ['databar', 'databar-exp', 'code128', 'code39', 'code93', 'i25', 'codabar',
+            'ean13', 'ean8', 'upca', 'upce', 'i25', 'qr'],
+        //symbologies: ['upca'],
+        numScanlines: 15,
+        minScanlinesNeeded: 2,
+        duplicateInterval: 1500
+    },
+    locator: {
+        regionOfInterest: {
+            left: 0.05, right: 0.05, top: 0.3, bottom: 0.3 // narrow RoE for 1D
+        }
+    },
+    frameSource: {
+        resolution: 'full-hd'
+    },
+    overlay: {
+        showCameraSelector: true,
+        showFlashlight: true,
+        showDetections: false
+    },
+    feedback: {
+        audio: true,
+        vibration: true
+    }
+};
+
+
+export function initializeSDK() {     
+    const license_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjZDIyYjI2MS1iNmI5LTRiOWMtOTIxNC0wNGZmYjY1ZjEwNWMiLCJpc3MiOiJzdHJpY2guaW8iLCJhdWQiOlsiaHR0cHM6Ly93d3cuaW5maW5lcmEuY29tLyJdLCJpYXQiOjE3MDgwMTQ1NTcsIm5iZiI6MTcwODAxNDU1NywiY2FwYWJpbGl0aWVzIjp7fSwidmVyc2lvbiI6MX0.9G4nSpbBp22y-4BQSGIfAEghdlZtBH_O9KUuY9ywUEk'    
+    import("https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.4.4")
+        .then((module1) => {module1.StrichSDK.initialize(license_key)})
+        .then(() => {
+            alert("SDK initialized successfully, ready to scan.");
+        })
+        .catch(err => {
+            alert('SDK initialization failed: ' + err.message);
+        });;
+}
+
+function get_detection(barcodeReader, detections) {
+    var message;
+    message = detections[0].data;    
+    console.log("the code: " + message);
+    stopScanning(barcodeReader, message);
+    return message;
+}
+export function startBarcodeScan(dotNetHelper1) {    
+    var msg_return = "start_scanning";
+    var barcodeReader = null;
+    var BCR = null;
+
+    if (barcodeReader) {
+        stopScanning(barcodeReader, null);
+    } else {
+        import("https://cdn.jsdelivr.net/npm/@pixelverse/strichjs-sdk@1.4.4")
+            .then((module2) => {                
+                BCR = new module2.BarcodeReader(defaultConfig4BCR);                
+                BCR.detected = async (detections) => {
+                    msg_return = await get_detection(barcodeReader, detections);
+                    await dotNetHelper1.invokeMethodAsync('UpdateCodeAsync', msg_return);
+                };
+                BCR.initialize().then(br => {
+                    barcodeReader = br;
+                    br.start();
+                }).catch(() => {
+                    resolve("Scanning failed!");
+                });               
+            });                                    
+    }
+}
+
+function stopScanning(barcodeReader, value) {
+    // destroy the barcodeReader in any case
+    barcodeReader.stop().then(() => {
+        barcodeReader.destroy();
+    }).then(() => {
+        barcodeReader = null;
+    });
+}
+
